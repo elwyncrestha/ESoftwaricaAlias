ed105db8cb3ec2a1cb1f1e1264644aafcd0abbaa
Elvin Shrestha <elwyncrestha@gmail.com>
2019-12-12 23:11:10 +0545

    Feat: Added `LoginActivity` and Removed AppBarConfig Codes
    ```
    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications).build();
    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
    ```

---
+++ app/src/main/java/com/elvin/esoftwaricaalias/ui/core/AppConstant.java
@@ -0,0 +1,11 @@
+package com.elvin.esoftwaricaalias.ui.core;
+
+/**
+ * @author Elvin Shrestha on 12/12/19
+ */
+public class AppConstant {
+
+    public static final String LOGIN_USERNAME = "softwarica";
+    public static final String LOGIN_PASSWORD = "coventry";
+
+}
