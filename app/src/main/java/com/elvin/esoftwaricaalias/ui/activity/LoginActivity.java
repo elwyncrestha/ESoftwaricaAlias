ce81d70455fc9d150b6e748b7dff898ac36d13cd
Elvin Shrestha <elwyncrestha@gmail.com>
2019-12-12 22:30:57 +0545

    Feat: Added Login Activity

---
+++ app/src/main/java/com/elvin/esoftwaricaalias/ui/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.elvin.esoftwaricaalias.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.elvin.esoftwaricaalias.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
