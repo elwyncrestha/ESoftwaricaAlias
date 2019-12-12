f413855ea6ccc2176b3d6b5e0a45d187b36b97e9
Elvin Shrestha <elwyncrestha@gmail.com>
2019-12-12 23:17:21 +0545

    Refactor: Remove ViewModels

--- app/src/main/java/com/elvin/esoftwaricaalias/ui/activity/MainActivity.java
+++ app/src/main/java/com/elvin/esoftwaricaalias/ui/activity/MainActivity.java
@@ -2,14 +2,13 @@ package com.elvin.esoftwaricaalias.ui.activity;
 
 import android.os.Bundle;
 
-import com.elvin.esoftwaricaalias.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.elvin.esoftwaricaalias.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
